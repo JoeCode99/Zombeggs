b8b8dc068cbd757e1acfb044ffa0be0286c33a01
Joseph <dacalculator@gmail.com>
2020-03-18 13:35:49 +1100

    Starting up...

---
+++ src/components/ListItem.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import '../index.css';
+import PropTypes from 'prop-types';
+import Tick from './Tick';
+import DropDown from './DropDown';
+import SubList from './SubList';
+
+const ListItem = ({content, step, lastStep, map} : { content: Array<string>, step: number, lastStep: number, map: string}) => {
+  const numColors: Array<string> = ['#F4AC45', '#2292A4', '#BDBF09', "#d5d80d"];
+  const showDropDown: boolean = content.length > 1;
+  const [complete, setComplete] = useState(false);
+  const [position, setPosition] = useState("right");
+  const handleClick = () => setComplete(prev => !prev);
+  const DropDownPosition = () => setPosition(prev => prev === "right" ? "down" : "right");
+  const allComplete = () => setComplete(true);
+
+  return (
+    <React.Fragment>
+      <div className="listContent" style={{marginBottom: complete ? '-45px' : '-15px', display: complete ? 'inline-block' : 'block', width: complete ? '80px' : '600px'}}>
+        <p id='step' onClick={handleClick} style={{backgroundColor: complete ? numColors[2] : step === lastStep ? numColors[3] : numColors[step%2], color: step === lastStep ? 'black' : 'white'}}>{complete ? <Tick /> : step}</p>
+        <div id="content" onClick={DropDownPosition} style={{cursor: showDropDown ? 'pointer' : 'auto'}}>
+          <p id="text-content" style={{display: complete ? 'none' : 'block'}}>{content[0]}</p>
+            <DropDown position={position} display={(complete || !showDropDown) ? 'none' : 'block'}/>
+        </div>
+      </div>
+      <div style={{display: (position === "down" && !complete) ? 'block' : 'none', marginLeft: '100px', marginTop: '40px'}}>
+          {!complete && <SubList content={content} allComplete={allComplete} map={map}/>}
+      </div>
+    </React.Fragment>
+  );
+}
+
+ListItem.propTypes = {
+  step: PropTypes.number.isRequired,
+  content: PropTypes.string.isRequired,
+};
+
+export default ListItem;
---
+++ src/components/SubList.tsx
@@ -0,0 +1,28 @@
+import React, {useState, useEffect} from 'react';
+import SubListItem from './SubListItem';
+import '../index.css';
+
+const List = ({content, allComplete, map} : { content: Array<string>, allComplete: any, map: string}) => {
+  const [numComp, setNumComp] = useState(0);
+  let count: number = 1;
+  const sliced = content.slice(1);
+
+  const handleComp = (completed: boolean) => {
+    completed ? setNumComp(prev => prev+1) 
+      : setNumComp(prev => prev-1);
+  };
+
+  const contentList = sliced.map(i => <SubListItem key={count} step={count++} content={i} completed={handleComp} map={map}/>);
+  
+  useEffect(() => {
+    if (numComp === sliced.length && numComp > 0) allComplete();
+  }, [numComp]);
+
+  return (
+    <div className="sublistContent">
+      {contentList}
+    </div>
+  );
+}
+
+export default List;
