186e0593e96b57e7f7d793236dd389206cd91467
JoeCode99 <dacalculator@gmail.com>
2021-07-23 15:39:29 +1000

    Mauer Der Toten map addition

--- src/components/common/GameDatabase.js
+++ src/components/common/GameDatabase.js
@@ -1,6 +1,7 @@
 const GameDatabase = new Map([
     ['Black_Ops_3', ['Der_Eisendrache']], 
-    ['Black_Ops_4', ['IX']]
+    ['Black_Ops_4', ['IX']],
+    ['Black_Ops:_Freezing_war', ['Mauer_Der_Toten']]
 ]);
 
 export default GameDatabase;
